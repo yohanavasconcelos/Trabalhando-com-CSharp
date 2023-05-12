880c92923c3be1fdcebba92f75dbc06f7346bdbe
yohanavasconcelos <125891632+yohanavasconcelos@users.noreply.github.com>
2023-05-12 10:21:07 -0400

    Add files via upload

---
+++ bytebank_ADM/bytebank_ADM/SistemaInterno/IAutenticavel.cs
@@ -0,0 +1,17 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.SistemaInterno
+{
+    public interface IAutenticavel 
+    {
+
+        public string Senha { get; set; }
+
+        public  bool Autenticar(string senha);
+    }
+}
---
+++ bytebank_ADM/bytebank_ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,16 @@
+﻿using bytebank_ADM.Funcionarios;
+
+namespace bytebank_ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonificacao += funcionario.GetBonificacao();
+        }
+
+      
+    }
+}
